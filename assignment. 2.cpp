71c5b41bc0c733a56e7783f9ca8888ef897db479
saim2007sajid-design <saim2007sajid@gmail.com>
2025-11-27 09:43:13 +0500

    Add files via upload

---
+++ assignment. 2.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"\n\***************menu**********";
+   cout<<"\n2.\t karachi \t\tRs\t600";
+   cout<<"\n3.\t Drinks\t\tRs.\t500";
+   cout<<"\n4.\tQorma\t\tRs.\t500";
+   cout<<"\n****************";
+   return 0;
+   
+   }
+   
+   
+   
+ 
+    
