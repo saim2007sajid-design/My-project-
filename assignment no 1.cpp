e2fae4bce7cfacbb6cf2d052b6d752646bbdc753
saim2007sajid-design <saim2007sajid@gmail.com>
2025-11-27 09:30:35 +0500

    Add files via upload

---
+++ assignment no 1.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"\n\t\t.....................";
+   cout<<"\n\t\t\student Id card";
+   cout<<"\n\t..................";
+   cout<<"\n\t Name :\t\t Raja  SaimSajid";
+   cout<<"\n\"Roll no:\\t 2025-SET-07"; 
+   cout<<"\n\tDepartment:\tSoftware engineering ";
+   cout<<"\n\t Session::\t 2025-2029";
+   cout<<"\n\t..................";
+   return 0;
+   }
+   
+   
+   
+ 
+    
