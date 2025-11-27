a2797e87001a8ee88b983cb3eb76054c0eb36bd9
saim2007sajid-design <saim2007sajid@gmail.com>
2025-11-27 09:44:36 +0500

    Add files via upload

---
+++ my affort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string Name, Department;
+    int Roll_Number,english_marks,math_marks,physics_marks;
+    cout<<"====================\n\n";
+    cout<<"\t Student result card\n\n";
+    cout<<"name;\t\t";
+    cin>>Name;
+    cout<<"Roll Number:\t";
+    cin>>Roll_Number;
+    cout<<"Department \t";
+    cin>>Department;
+    cout<< "==============\n";
+    cout<<"Subject\t\t\Marks\n\n";
+    cout<<"===================\n";
+    cout<<"english\t\t";
+    cin>> english_marks;
+    cout<<"math\t\t";
+    cin>>math_marks;
+    cout<<"physics \t\t";
+    cin>>physics_marks;
+    cout<<"===================\n";
+    int sum=english_marks+math_marks+physics_marks;
+    cout<<"Total Marks:\t<<Sum<<endl";
+    cout<<"percentage \t85\n\n";
+    cout<<"=================\n";
+    cout<<"Grade:\tA+\n";
+    cout<<"==============\n";
+    cout<<" congratulations Saim Sajid\n";
+    cout"==============\n";
+    return 0;
+    
+    
+    
+    
+    
+    
+    
+}
