b284690861547734db0ebaf0984a9fb0fb5bea96
saim2007sajid-design <saim2007sajid@gmail.com>
2025-11-27 09:43:50 +0500

    Add files via upload

---
+++ aaa.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main()
+{
+float base,height,area;
+std::cout<<"enter base and height";
+std::cin>>base>>height;
+area=0.5*(base*height);
+std::cout<<"Area of triangle"<<area;
+}
+
+
+
+
+
+
+    
